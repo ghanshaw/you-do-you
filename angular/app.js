6d6e28fc66ae91ef3d49df91877f191636c01687
Greer Hanshaw <greer.hanshaw@gmail.com>
2017-06-18 22:41:36 -0400

    major frontend improvements

--- angular/app.js
+++ angular/app.js
@@ -1,4 +1,4 @@
-var ydyApp = angular.module('ydyApp', ['ngRoute']);
+var ydyApp = angular.module('ydyApp', ['ngRoute', 'ngMaterial', '720kb.datepicker']);
 
 // Route view based on location path
 ydyApp.config(function ($routeProvider, $locationProvider) {
@@ -32,22 +32,202 @@ ydyApp.config(function ($routeProvider, $locationProvider) {
 
 ydyApp.controller('backgroundController', function($scope, $http) {
 
+
 });
 
 ydyApp.controller('loginController', function($scope, $http) {
 
+  $scope.authentication = {
+    login: true,
+    signup: false
+  }
+
+  $scope.toggleAuthentication = function(form) {
+
+    if (form === 'login') {
+      $scope.authentication.login = true;
+      $scope.authentication.signup = false;
+    }
+
+    else if (form === 'signup') {
+      $scope.authentication.login = false;
+      $scope.authentication.signup = true;
+    }
+  }
+
+});
+
+
+ydyApp.controller('mainController', function($scope, $http, $window) {
+
+  console.log('kjasdjfkasdf');
+
+  $scope.windowScroll = {
+    off: false
+  }
+
+
+
+  $scope.images = [
+    {
+      thumb: 'images/backgrounds/city-thumb.jpg',
+      url: 'images/backgrounds/city.jpg'
+    },
+    {
+      thumb: 'images/backgrounds/lake-thumb.jpg',
+      url: 'images/backgrounds/lake.jpg'
+    },
+    {
+      thumb: 'images/backgrounds/rockies-thumb.jpg',
+      url: 'images/backgrounds/rockies.jpg'
+    },
+    {
+      thumb: 'images/backgrounds/snow-thumb.jpg',
+      url: 'images/backgrounds/snow.jpg'
+    },
+    {
+      thumb: 'images/backgrounds/space-thumb.jpg',
+      url: 'images/backgrounds/space.jpg'
+    },
+    {
+      thumb: 'images/backgrounds/waterfall-thumb.jpg',
+      url: 'images/backgrounds/waterfall.jpg'
+    },
+  ]
+
+  $scope.collapseMenu = {
+    open: false,
+    element: $('div.navbar-collapse'),
+    offset: 0,
+    height: 0
+  }
+
+
+  $scope.backgroundMenu = {
+    open: false,
+  }
+  
+
+  $scope.positionBackgroundMenu = function() {
+
+    // if (!$scope.collapseMenu.open) return;      
+    
+    $scope.collapseMenu.offset = $($scope.collapseMenu.element).offset().top;
+    $scope.collapseMenu.height = $($scope.collapseMenu.element).height();
+
+    var backgroundMenu = $('div.user-select-background');
+    $(backgroundMenu).css('top', $scope.collapseMenu.offset + $scope.collapseMenu.height + 10);
+
+  }
+
+  $scope.toggleBackgroundMenu = function() {
+    $scope.positionBackgroundMenu();
+    $scope.backgroundMenu.open = !$scope.backgroundMenu.open;
+  }
+
+  $scope.closeBackgroundMenu = function() {
+    $scope.backgroundMenu.open = false;
+  }
+
+  $scope.changeBackgroundImage = function($index) {
+
+    var url = $scope.images[$index].url;
+    var property = 'url(' + url + ')';
+
+    $('.background .user-choice').css('background', property);
+
+    //background: url('../images/backgrounds/space.jpg');
+
+
+  }
+
+
+  angular.element($window).bind("resize", function(event) {
+    
+    // console.log($window);
+
+    // If window is size xs
+    if ($window.innerWidth <= 768) {
+      $scope.backgroundMenu.open = false;
+      $scope.collapseMenu.open = true;
+      $scope.$apply();
+    }      
+
+    $scope.positionBackgroundMenu();
+             
+  });
+
+  // $(window).trigger('resize');
+
+  
+
+  $scope.checkWindow = function() {
+    console.log($scope.windowScroll) ;
+  }
+
+  $scope.about = {
+    open: false
+  };
+
+  $scope.closeAbout = function($event) {
+
+    // console.log($($event.target).is('div.about-modal'));
+
+    //$event.stopPropagation();
+
+    // if ($($event.target).is('div.about-modal')) { return; }
+
+    console.log($event)
+    $scope.about.open = false;
+  }
+
+  $scope.openAbout = function() {
+    $scope.about.open = true;
+  }
+
+
+
+
 });
 
 
 
-ydyApp.controller('todosController', function($scope, $http, $filter, $window) {
+ydyApp.controller('todosController', function($scope, $http, $filter, $window, $interval) {
 
-  
+  // $( function() {
+  //   $( "#datepicker" ).datepicker();
+  // } );
+
+  // $scope.myDate = new Date();
+  // $scope.isOpen = false;
+
+  // $(window).click(function() {
+  //   console.log('clicked');
+  //   for (t of $scope.todos) {
+  //     console.log(t);
+  //     t.editing = false;
+  //   }
+  //   $scope.$apply();
+  // });
+
+
+
+  //$('[data-toggle="datepicker"]').datepicker();
+
 
   // Get date and time
-  var date = Date.now();
-  $scope.date = $filter('date')(date, 'EEEE, MMMM d');
-  $scope.time = $filter('date')(date, 'shortTime');
+  
+  $scope.date = '';
+  $scope.time = '';
+
+  var updateClock = $interval(function() {
+
+    // var date = Date.now();
+    $scope.date = $filter('date')(Date.now(), 'EEEE, MMMM d');
+    $scope.time = $filter('date')(Date.now(), 'shortTime');
+
+  }, 30);
+
 
   // Add date suffix
   var suffixes = ["th", "st", "nd", "rd"];
@@ -62,6 +242,18 @@ ydyApp.controller('todosController', function($scope, $http, $filter, $window) {
 
   $scope.newTaskContent = '';
 
+  $scope.enterTask = function(todo) {
+    todo.hovering = true;
+  }
+
+  // Set hovering to false, end editing
+  $scope.leaveTask = function(todo) {
+    todo.hovering = false;
+    todo.editing = false;
+  }
+
+
+
   $scope.addTask = function() {
 
     if ($scope.newTaskContent == '') return;
@@ -71,6 +263,9 @@ ydyApp.controller('todosController', function($scope, $http, $filter, $window) {
     var todo = {
       isDone: false, 
       content: $scope.newTaskContent,
+      id: Math.random() * 10,
+      dateCreated: Date.now(),
+      dueDate: null
     }
 
     $scope.newTaskContent = '';
@@ -151,8 +346,11 @@ ydyApp.controller('todosController', function($scope, $http, $filter, $window) {
   }
 
   var listWindow = $('.list-task .window');
-  angular.element(listWindow).bind("scroll", function() {
-    
+  angular.element(listWindow).bind("scroll", function(event) {
+
+        
+
+
         $scope.atTop();
       $scope.atBottom();
     $scope.$apply();
@@ -160,6 +358,72 @@ ydyApp.controller('todosController', function($scope, $http, $filter, $window) {
              
   });
 
+
+
+  $scope.stopWindowScroll = function() {
+
+    $scope.windowScroll.off = true;
+
+  }
+
+  $scope.resumeWindowScroll = function() {
+
+    $scope.windowScroll.off = false;
+
+  }
+
+
+
+  var date = new Date();
+  date.setDate(date.getDate() - 1);
+  console.log(date.toString())
+  $('datepicker').attr('date-min-limit', date.toString());
+
+  $scope.editTodo = function(todo) {
+    if (todo.isDone) return;
+    todo.editing = true;
+  }
+
+
+  $scope.stopEditTodo = function(todo) {
+    var d = new Date(todo.dueDate);
+    todo.dueDateNum = d.getTime();
+    todo.editing = false;
+    console.log(todo);
+  }
+
+
+  $scope.keyStopEditTodo = function($event, todo) {
+
+    var keyCode = $event.keyCode;
+
+    // Enter -> 13, Escape -> 27
+    if (keyCode === 13 || keyCode === 27) {
+      $scope.stopEditTodo(todo);
+    }
+  }
+
+  $scope.deleteTodo = function(todo) {
+    var index = $scope.todos.findIndex(function(t, i) {
+      return t.id == todo.id;
+    });
+    $scope.todos.splice(index, 1);
+  }
+
+  // Determine if datepicker is closer to top or bottom of window
+  $scope.closerToTop = function($index) {
+
+    // console.log($('datepicker')[$index]);
+
+    var element = $('datepicker')[$index];
+
+    var elementTop = $(element).offset().top;
+    var listWindowTop = $('.list-task .window').offset().top;
+    var listWindowBottom = listWindowTop + $('.list-task .window').height();
+
+    return (listWindowTop - elementTop) < (elementTop - listWindowBottom)
+
+  }
   
 
   console.log($scope.date);
@@ -169,14 +433,24 @@ ydyApp.controller('todosController', function($scope, $http, $filter, $window) {
     {
       isDone: false,
       content: 'Get milk',
+      dueDate: null,
+      dateCreated: 1497802960006,
+      id: 001
     },
     {
       isDone: true,
       content: 'Buy eggs.',
+      dueDate: null,
+      dateCreated: 1497802932306,
+      id: 002
     },
+    // Oldest
     {
       isDone: false,
       content: 'Break bread.',
+      dueDate: null,
+      dateCreated: 297802962306,
+      id: 003
     }
   ]
 
@@ -185,7 +459,7 @@ ydyApp.controller('todosController', function($scope, $http, $filter, $window) {
 
 
 
-ydyApp.controller('mainController', function ($scope, $http, $interval) {
+ydyApp.controller('oldMainController', function ($scope, $http, $interval) {
   // return;
   // console.log('kajskdfjaksf');
 
