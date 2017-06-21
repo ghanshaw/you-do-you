e916e469614052d6b766422d4f7c4343e39abd63
Greer Hanshaw <greer.hanshaw@gmail.com>
2017-06-21 12:42:44 -0400

    deploying to heroku

--- app.js
+++ app.js
@@ -127,7 +127,7 @@ app.use('/background', background);
 mongoose.connect(config.getDbConnectionString());
 
 // Listen on port
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 8080));
 app.listen(app.get('port'), function() {
     console.log('Server started on port ' + app.get('port'));
 });
